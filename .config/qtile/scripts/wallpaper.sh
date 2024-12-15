7f7a5003edf236d7db0673a747971ba2a021e14d
Kirill <2spybot@gmail.com>
2024-12-15 17:03:55 +0300

    picom cmd edited

--- .config/qtile/scripts/wallpaper.sh
+++ .config/qtile/scripts/wallpaper.sh
@@ -2,11 +2,12 @@
 set -o nounset  # exit on uninitialised variable
 set -o errexit  # exit on error
 #set -o xtrace   # debug mode
-setxkbmap -option grp:ralt_rshift_toggle us,ru &
+# setxkbmap -option grp:ralt_rshift_toggle us,ru &
 setxkbmap -option grp:caps_toggle us,ru &
 
 # sxhkd &
-picom --transparent-clipping &
+# picom --transparent-clipping &
+picom &
 opensnitch-ui &
 feh --bg-fill ~/Pictures/walls/3.jpg &
 # feh --bg-fill -z ~/Pictures/walls
