afbb9767b4a0163502c8ee74f80a25bad81fe4ef
Kirill <2spybot@gmail.com>
2025-09-21 21:08:10 +0300

    fish config added

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    atuin init fish | source
+end
+
+zoxide init fish | source
+
+alias ls='eza -lah --total-size --icons=auto --group-directories-first'
+alias lt='eza --tree --group-directories-first --icons --git-ignore'
+alias ll='eza -la --group-directories-first --icons'
+
+alias yy='yazi'
+alias ff='fastfetch --raw /home/rep/Pictures/image.bin --logo-width 15'
+
+# alias pbpaste='xclip -selection clipboard -o'
+
+# alias wgup="sudo wg-quick up wg0"
+# alias wgdn="sudo wg-quick down wg0"
+
+alias gdriv='rclone copy "/home/rep/Documents/DATA_TEMP/other/сбор данных/pwd.kdbx" gdrive:backup'
+alias dropb='rclone copy "/home/rep/Documents/DATA_TEMP/other/сбор данных/pwd.kdbx" remote:backup'
